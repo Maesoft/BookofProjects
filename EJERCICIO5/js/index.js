4cf3f783ef0342929f1aab1dd4020931c50b7ba3
Matias Ariel Eguia <matias_eguia@hotmail.com>
2022-10-26 13:13:28 -0300

    Trabajo Terminado

---
+++ EJERCICIO5/js/index.js
@@ -0,0 +1,33 @@
+const btn=document.getElementById("btn");
+const btn2=document.getElementById("btn2");
+const btn3=document.getElementById("btn3");
+const resultado=document.getElementById("resultado");
+let contador=0;
+let mensaje=false;
+
+btn.addEventListener("click", ()=>{
+    contador++;
+    resultado.innerText=contador;
+    if(mensaje==false && contador>=50){
+        alert("Usted llego a la mitad de los clicks.");
+        mensaje=true;
+    }
+    if(contador>=100){
+        resultado.innerText="Usted llego al final del juego por hacer 100 clicks."
+    }
+})
+btn2.addEventListener("click", ()=>{
+    contador+=5;
+    resultado.innerText=contador;
+    if(mensaje==false && contador>=50){
+        alert("Usted llego a la mitad de los clicks.");
+        mensaje=true;
+    }
+    if(contador>=100){
+        resultado.innerText="Usted llego al final del juego por hacer 100 clicks."
+    }
+})
+btn3.addEventListener("click", ()=>{
+    contador=0;
+    resultado.innerText=contador;
+})
